1ded187dcfc30b2abd7cb51a0a14a0222b7d8f0b
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-08-03 03:29:22 +0800

    Update stun_hath.sh

--- stun_hath.sh
+++ stun_hath.sh
@@ -1,11 +1,11 @@
 # 以下变量需按要求填写
-PROXY='http://jpfhDg:qawsedrftgyhujikolp@hathproxy.ydns.eu:14913'
-IFNAME=
-HATHDIR=/mnt/hath
-HATHCID=48281
-EHIPBID=3809340
-EHIPBPW=02a8cd074101c9d60ce793a1468ba07a
-GWLADDR=192.168.97.1
+PROXY='socks5://192.168.1.168:10808'		# 可用的代理协议、地址与端口
+IFNAME=										# 指定接口，可留空；仅在多 WAN 时需要；拨号接口的格式为 "pppoe-wancm"
+GWLADDR=192.168.1.1							# 主路由 LAN 的 IPv4 地址
+HATHDIR=/mnt/hath							# H@H 所在目录
+HATHCID=12345								# H@H 的客户端 ID
+EHIPBID=1234567								# ipb_member_id
+EHIPBPW=0123456789abcdef0123456789abcdef	# ipb_pass_hash
 
 WANADDR=$1
 WANPORT=$2
