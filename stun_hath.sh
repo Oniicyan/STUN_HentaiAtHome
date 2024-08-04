4c7abdd3cf16ddd0262b299c83ef089d4e78b733
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-08-04 20:13:00 +0800

    Update stun_hath.sh

--- stun_hath.sh
+++ stun_hath.sh
@@ -156,7 +156,7 @@ RUNHATH
 # 检测启动结果
 while :; do
 	sleep 60
-	grep "Startup notification failed" $HATHLOG || { screen -S $OWNNAME -X log off; exit; }
+	grep "Startup notification failed" $HATHLOG || break
 	if grep "port $WANPORT" $HATHLOG; then
 		sleep 300
 		RUNHATH
@@ -166,4 +166,5 @@ while :; do
 	fi
 done
 
+screen -S $OWNNAME -X log off
 echo -n HentaiAtHome OK.
